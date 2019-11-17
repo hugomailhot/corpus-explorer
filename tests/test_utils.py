0dace9fe1e885e474ddc574deba49c4aa37711ab
Hugo Mailhot <mailhot.hugo@gmail.com>
2019-11-17 15:13:36 -0800

    Added unit test for get_topic_proportions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -27,3 +27,18 @@ def test_normalize_text_removes_punctuation():
     raw_text = 'that guy: he really takes the "the" out of "psychotherapist", amirite?'
     expected = 'that guy he really takes the the out of psychotherapist amirite'
     assert normalize_text(raw_text) == expected
+
+
+def test_get_topic_proportion_return_correction_proportions():
+    doctopics = np.array([
+        [0.2, 0.3, 0.5],
+        [0.4, 0.2, 0.4],
+    ])
+    doclengths = np.array([2, 3])
+
+    expected = np.array([0.32, 0.24, 0.44])
+    actual = get_topic_proportions(doctopics, doclengths)
+
+    # Using allclose instead of array_equal here, since floating point
+    # imprecision causes inequality with a difference of 5e-17
+    assert np.allclose(actual, expected)
